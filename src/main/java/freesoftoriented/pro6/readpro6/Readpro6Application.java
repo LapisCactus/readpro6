7e44d1705df7be51c92ed0e8280bd3019a2a835d
Akihiko Nakagawa <fsoriented@gmail.com>
2020-06-12 01:37:08 +0900

    initial commit

---
+++ src/main/java/freesoftoriented/pro6/readpro6/Readpro6Application.java
@@ -0,0 +1,13 @@
+package freesoftoriented.pro6.readpro6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Readpro6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Readpro6Application.class, args);
+	}
+
+}
