7b7b91288381587f4ac19c94d4b4123579671cd4
Akihiko Nakagawa <fsoriented@gmail.com>
2020-06-13 11:57:15 +0900

    Edit RTF part and back to xml file succeeded

--- src/main/java/freesoftoriented/pro6/readpro6/ProPresentor6Data.java
+++ src/main/java/freesoftoriented/pro6/readpro6/ProPresentor6Data.java
@@ -1,13 +1,19 @@
 package freesoftoriented.pro6.readpro6;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import javax.xml.bind.JAXB;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlValue;
 
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.stereotype.Service;
 
 /**
@@ -22,22 +28,84 @@ public class ProPresentor6Data {
 		// XML File→JavaObject
 		System.out.println("\nConvert:");
 		RVPresentationDocument data = readFromFile(filepath);
-		System.out.println(data);
 		// Edit
-		System.out.println(String.format("Slide size: %s x %s", data.getWidth(), data.getHeight()));
+		System.out.println(String.format("\nSlide size: %s x %s", data.getWidth(), data.getHeight()));
 		List<RVSlideGrouping> slideGroup = data.findSlideGroup();
 		for (RVSlideGrouping group : slideGroup) {
 			List<RVDisplaySlide> slides = group.findDisplaySlide();
 			System.out.println(String.format("Slide Group (%s slides)", slides.size()));
 			for (RVDisplaySlide slide : slides) {
 				List<RVTextElement> elements = slide.findTextElement();
-				System.out.println(String.format("- Slide (%s text area)", elements.size()));
+				System.out.println(String.format("\n- Slide (%s text area)", elements.size()));
+				int[] sizearray = { 116, 100, 100 };
+				if (elements.size() == 1) {
+					sizearray[0] = 138;
+				}
+				for (int i = 0; i < elements.size(); i++) {
+					RVTextElement element = elements.get(i);
+					handleTextElement(element, sizearray[i]);
+				}
 			}
 		}
 
 		// JavaObject→XML
 		System.out.println("\n\nBack to XML:");
-		JAXB.marshal(data, System.out);
+		writeToFile(filepath + "_out.pro6", data);
+	}
+
+	private void handleTextElement(RVTextElement te, int size) {
+		List<Integer> position = te.extractPosition();
+		int ul_x = position.get(0);
+		int ul_y = position.get(1);
+		int lr_x = position.get(3);
+		int lr_y = position.get(4);
+		System.out.println(String.format("  - Text (%d,%d) w%d h%d", ul_x, ul_y, lr_x - ul_x, lr_y - ul_y));
+		String rawRTFData = te.findRawRTFData();
+		RTFEditor rtf = new RTFEditor(rawRTFData);
+		rtf.setFontSize(size);
+		te.replaceRawRTFData(rtf.getRtfText());
+		System.out.println(rtf.getRtfText());
+	}
+
+	/**
+	 * RTF部分を編集する
+	 *
+	 */
+	@lombok.ToString(exclude = { "rtfText" })
+	public static class RTFEditor {
+
+		@lombok.Getter
+		private String rtfText;
+
+		public RTFEditor(String rtftext) {
+			this.rtfText = rtftext;
+		}
+
+		public void setFontSize(int size) {
+			// フォントサイズはファイル内部では2倍になっている
+			String[] segments = rtfText.split("\\\\fs");
+			if (segments == null || segments.length == 0) {
+				System.out.println("[INFO] nothing to do");
+				return;
+			}
+			// \fsコマンドで区切って、サイズ部分を上書き
+			List<String> list = new ArrayList<>();
+			list.add(segments[0]);
+			Pattern pattern = Pattern.compile("([0-9]+)(.*)", Pattern.DOTALL);
+			for (int i = 1; i < segments.length; i++) {
+				Matcher matcher = pattern.matcher(segments[i]);
+				if (!matcher.matches()) {
+					System.out.println("[ERROR] Cannot edit");
+					return;
+				}
+				list.add(String.format("%d%s", size * 2, matcher.group(2)));
+			}
+			rtfText = String.join("\\fs", list);
+		}
+
+		public void updateColorTable() {
+			// カラーテーブルを更新する
+		}
 	}
 
 	/**
@@ -48,6 +116,16 @@ public class ProPresentor6Data {
 	 */
 	public static RVPresentationDocument readFromFile(String filepath) {
 		return JAXB.unmarshal(new File(filepath), RVPresentationDocument.class);
+	}
+
+	/**
+	 * Pro6ファイルを書き込む
+	 * 
+	 * @param filepath
+	 * @param doc
+	 */
+	public static void writeToFile(String filepath, RVPresentationDocument doc) {
+		JAXB.marshal(doc, new File(filepath));
 	}
 
 	/**
@@ -342,6 +420,51 @@ public class ProPresentor6Data {
 		@XmlElement(name = "NSString")
 		private P6KeyValue nsString;
 
+		/**
+		 * このテキストエリアの矩形座標を返す。<br/>
+		 * 左上x, y, z、右下x, y<br/>
+		 * 
+		 * <pre>
+		 * {7 7 0 2202 672}
+		 * (7,7,0)(2202,672)
+		 * </pre>
+		 * 
+		 * @return
+		 */
+		public List<Integer> extractPosition() {
+			if (rvRect3D == null) {
+				return null;
+			}
+			String value = rvRect3D.getValue();
+			// ex. {7 7 0 2202 672}
+			String[] nums = value.replace("{", "").replace("}", "").split(" ");
+			List<Integer> position = Arrays.asList(nums).stream().map(num -> {
+				return Integer.parseInt(num);
+			}).collect(Collectors.toList());
+			return position;
+		}
+
+		/**
+		 * このテキストエリアのRTF(Rich Text Format)データ。
+		 * 
+		 * @return
+		 */
+		public String findRawRTFData() {
+			String base64rtf = nsString.getValue();
+			byte[] decoded = Base64.decodeBase64(base64rtf);
+			return new String(decoded);
+		}
+
+		/**
+		 * このテキストエリアのRTFを、指定のもので置き換える
+		 * 
+		 * @param rtfData
+		 */
+		public void replaceRawRTFData(String rtfData) {
+			byte[] encoded = Base64.encodeBase64(rtfData.getBytes());
+			nsString = new P6KeyValue(nsString.getRvXMLIvarName(), new String(encoded));
+		}
+
 	}
 
 	/**
@@ -394,6 +517,8 @@ public class ProPresentor6Data {
 	 */
 	@lombok.ToString
 	@lombok.Getter
+	@lombok.NoArgsConstructor
+	@lombok.AllArgsConstructor
 	public static class P6KeyValue {
 
 		@XmlAttribute(name = "rvXMLIvarName")
